308a8c4093a9c727b083d4b0708c0c0412eafbad
ryun-kim <kjr6260@naver.com>
2021-11-02 16:59:06 +0900

    21-11-02

--- source/com/koreait/server/BoardDAO.java
+++ source/com/koreait/server/BoardDAO.java
@@ -2,6 +2,9 @@ package com.koreait.server;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BoardDAO {
     /*
@@ -36,6 +39,103 @@ public class BoardDAO {
         }catch(Exception e){
             e.printStackTrace();
         }finally{
+            DbUtils.close(con, ps);
+        }
+        return 0;
+    }
+    public static List<BoardVO> selBoardList() {
+        List<BoardVO> list = new ArrayList();
+
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        String sql = "SELECT iboard, title, writer, rdt " +
+                " FROM t_board ";
+        try {
+            con = DbUtils.getCon();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+
+            while(rs.next()) {
+                BoardVO vo = new BoardVO();
+                vo.setIboard(rs.getInt("iboard"));
+                vo.setTitle(rs.getString("title"));
+                vo.setWriter(rs.getString("writer"));
+                vo.setRdt(rs.getString("rdt"));
+                list.add(vo);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            DbUtils.close(con, ps, rs);
+        }
+        return list;
+    }
+
+    public static BoardVO selBoard(BoardVO param){
+        BoardVO vo = null;
+        Connection con = null;
+        PreparedStatement ps =null;
+        ResultSet rs = null;
+        String sql  = "SELECT * FROM t_board where iboard = ?";
+        try {
+            con = DbUtils.getCon();
+            ps = con.prepareStatement(sql);
+            ps.setInt(1,param.getIboard());
+            rs = ps.executeQuery();
+            if(rs.next()){
+                vo = new BoardVO();
+                vo.setIboard(rs.getInt("iboard"));
+                vo.setTitle(rs.getString("title"));
+                vo.setCtnt(rs.getString("ctnt"));
+                vo.setWriter(rs.getString("writer"));
+                vo.setRdt(rs.getString("rdt"));
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            DbUtils.close(con,ps,rs);
+        }
+        return vo;
+    }
+
+
+    public static int updBoard(BoardVO param) {
+        Connection con = null;
+        PreparedStatement ps = null;
+        String sql = " UPDATE t_board " +
+                " SET title = ? " +
+                " , ctnt = ? " +
+                " , writer = ? " +
+                " WHERE iboard = ? ";
+        try {
+            con = DbUtils.getCon();
+            ps = con.prepareStatement(sql);
+            ps.setString(1, param.getTitle());
+            ps.setString(2, param.getCtnt());
+            ps.setString(3, param.getWriter());
+            ps.setInt(4, param.getIboard());
+            return ps.executeUpdate();
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            DbUtils.close(con, ps);
+        }
+        return 0;
+    }
+
+    public static int delBoard(BoardVO param) {
+        Connection con = null;
+        PreparedStatement ps = null;
+        String sql = " DELETE FROM t_board WHERE iboard = ? ";
+        try {
+            con = DbUtils.getCon();
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, param.getIboard());
+            return ps.executeUpdate();
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
             DbUtils.close(con, ps);
         }
         return 0;
